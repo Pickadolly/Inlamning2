ff1bbcafcd66b6f855d6aaf82c9a782920577f57
Helena Johansson <helena@bulle.nu>
2018-11-27 13:22:56 +0000

    Finished the Guessing Game class so it works with user input

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class GuessingGame {
     public static void main(String[] args) {
 
-        Guesser a = new Guesser(0, 1000);
-        a.start();
+        //Guesser a = new Guesser(0, 1000);
+        //a.start();
+
+        Scanner sc = new Scanner(System.in);
+        int answer1;
+        int answer2;
+
+        System.out.println("Enter the the lowest number of the interval: ");
+        answer1 = sc.nextInt();
+
+        System.out.println("Enter the highest number of the interval: ");
+        answer2 = sc.nextInt();
+
+        Guesser g = new Guesser(answer1, answer2);
+        g.start();
+
 
     }
 }
